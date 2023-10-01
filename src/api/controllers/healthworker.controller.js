79d3cecccc5f6cd6d34b7d7bee06c1177fb53ab8
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-10-01 11:34:14 +0800

    refactors: modified  auth response format

--- src/api/controllers/healthworker.controller.js
+++ src/api/controllers/healthworker.controller.js
@@ -7,31 +7,20 @@ const signup = async (req, res) => {
 	const hashedPassword = bcrypt.hashSync(password, 8);
 
 	try {
-		const userExists = await HealthWorker.find({ hwid });
-
-		if (userExists) {
-			res.status(401).json({
-				status: 'error',
-				message: 'ID is taken already.',
-			});
-		}
-
-		await HealthWorker.create({
+		const newHealthWorker = await HealthWorker.create({
 			name: name,
 			healthWorkerId: hwid,
 			password: hashedPassword,
-		}).then((_res) => {
-			if (_res) {
-				res.status(201).json({
-					status: 'success',
-					message: 'Health worker created successfully.',
-				});
-			}
+		});
+
+		return res.status(201).json({
+			status: 'success',
+			message: 'Health worker created successfully.',
 		});
 	} catch (error) {
-		res.status(400).json({
+		return res.status(400).json({
 			status: 'error',
-			message: 'Failed to create health worker.',
+			message: error.message,
 		});
 	}
 };
@@ -41,25 +30,23 @@ const login = async (req, res) => {
 	const user = await HealthWorker.findOne({ healthWorkerId: hwid });
 
 	try {
-		if (!user) {
-			return res.status(404).json({
-				status: 'error',
-				message: 'Healthworker not found.',
-			});
-		}
-
 		const passwordMatched = bcrypt.compareSync(password, user.password);
 
 		if (passwordMatched) {
-			res.status(200).json({
+			return res.status(200).json({
 				status: 'success',
 				message: 'Login successful.',
 			});
 		}
+
+		return res.status(401).json({
+			status: 'error',
+			message: 'Login failed.',
+		});
 	} catch (error) {
 		res.status(400).json({
 			status: 'error',
-			message: 'Login failed.',
+			message: error.message,
 		});
 	}
 };
