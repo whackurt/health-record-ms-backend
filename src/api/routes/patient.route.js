cf4b1a17445d336c0d67affda63fe307165353d7
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-10-31 12:22:37 +0800

    refactor: modified order of patient routes

--- src/api/routes/patient.route.js
+++ src/api/routes/patient.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patient.controller');
 
+router.get('/:patientId', patientController.getPatient);
 router.get('/', patientController.getPatients);
 router.post('/', patientController.createPatient);
-router.get('/:patientId', patientController.getPatient);
 router.delete('/:patientId', patientController.deletePatient);
 router.put('/:patientId', patientController.updatePatient);
 
