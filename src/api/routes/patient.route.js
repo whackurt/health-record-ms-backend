cb0e5a2a42967d2455f8b9bf851a4cecd2d06099
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-11-09 11:52:49 +0800

    feat: implemented token-based private routes

--- src/api/routes/patient.route.js
+++ src/api/routes/patient.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patient.controller');
+const { verifyToken } = require('../middlewares/verifyToken');
 
-router.get('/:patientId', patientController.getPatient);
-router.get('/', patientController.getPatients);
-router.post('/', patientController.createPatient);
-router.delete('/:patientId', patientController.deletePatient);
-router.put('/:patientId', patientController.updatePatient);
+router.get('/:patientId', verifyToken, patientController.getPatient);
+router.get('/', verifyToken, patientController.getPatients);
+router.post('/', verifyToken, patientController.createPatient);
+router.delete('/:patientId', verifyToken, patientController.deletePatient);
+router.put('/:patientId', verifyToken, patientController.updatePatient);
 
 module.exports = router;
